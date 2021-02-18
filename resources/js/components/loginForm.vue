919d395009c5b0f6fb5cba17a3d3757c4e024c14
Raul Suria <raul.suria@coreproc.ph>
2021-02-18 13:02:35 +0800

    vue-version component for loginForm is not working

---
+++ resources/js/components/loginForm.vue
@@ -0,0 +1,8 @@
+<template>
+  <form>
+    <input type="text" placeholder="username">
+    <input type="password" placeholder="password">
+    <input type="submit" value="login">
+    <span>Not registered ? <a href="">Create an account</a></span>
+  </form>
+</template>
